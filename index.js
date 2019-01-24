3b8a9c9cdcf61a6784fef36110a5331e1dfa78f8
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-24 04:03:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  let array= []
+  for(let i=0;i<arrayMusicians.length; i++){
+    array.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var i= 0;
+  var array = [];
+  while(i<facts.length){
+    array.push(facts[i] + '!!!');
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array =[]
+  do{
+    array.push("I love the Beatles!");
+     number++
+  }
+  while (number<15);
+    return array
+}
